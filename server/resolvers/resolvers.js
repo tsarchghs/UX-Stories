059fcf926b5df82d57dcb6148c53a21e91be2c6e
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-07 15:42:25 +0100

    Created createAppVersion resolver

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -14,7 +14,7 @@ module.exports = {
 	Query: {
 		login: async (root,args,context) => {
 			if (!args.email || !args.password){
-				throw new Error("Either email or password is missing/empty");
+				throw new Error("Either email or password is empty");
 			}
 			const user = await context.prisma.user({email:args.email});
 			if (!user){
@@ -35,7 +35,7 @@ module.exports = {
 		signUp: async (root,args,context) => {
 			console.log(args);
 			if (!args.email || !args.name || !args.password){
-				throw new Error("Either email,name or password is missing/empty");
+				throw new Error("Either email,name or password is empty");
 			}
 			const hashed_password = await bcrypt.hash(args.password,saltRounds);
 			const user = await context.prisma.createUser({
@@ -51,17 +51,27 @@ module.exports = {
 			};
 		},
 		createAppCategory: async (root,args,context) => {
-			if (!args.name) {
-				throw new Error("name argument is missing/empty")
+			if (!args.appCategoryInput.name) {
+				throw new Error("appCategoryInput.name argument is empty")
 			}
-			const exists = await context.prisma.appCategories({where:{name:args.name}});
+			const exists = await context.prisma.appCategories({where:{name:args.appCategoryInput.name}});
 			if (exists.length){
 				throw new Error("Category already exists (use different name)");
 			}
 			const appCategory = await context.prisma.createAppCategory({
-				name: args.name
+				name: args.appCategoryInput.name
 			})
 			return appCategory
+		},
+		createAppVersion: async (root,args,context) => {
+			if (!args.appVersionInput.version) {
+				throw new Error("appCategoryInput.version argument is empty")
+			}
+			const appVersion = await context.prisma.createAppVersion({
+				version: args.appVersionInput.version,
+				order: args.appVersionInput.order,
+			})
+			return appVersion
 		}
 	}
 }
