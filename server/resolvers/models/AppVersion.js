2ed8759b0a21a7855424d2b60d8f0dd2290ee0f4
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-11 23:17:47 +0100

    Created editAppVersion resolver

--- server/resolvers/models/AppVersion.js
+++ server/resolvers/models/AppVersion.js
@@ -2,15 +2,29 @@
 
 const createAppVersion = async (root,args,context) => {
 	if (!args.appVersionInput.version) {
-		throw new Error("appCategoryInput.version argument is empty")
+		throw new Error("version/id argument is empty")
 	}
 	const appVersion = await context.prisma.createAppVersion({
 		version: args.appVersionInput.version,
-		order: args.appVersionInput.order,
 	})
 	return appVersion
 }
 
+const editAppVersion = async (root,args,context) => {
+	if (!args.editAppVersionInput.version || !args.editAppVersionInput.id) {
+		throw new Error("version/id argument is empty")
+	}
+	const appVersion = await context.prisma.updateAppVersion({
+		where: {
+			id: args.editAppVersionInput.id
+		},
+		data: {
+			version: args.editAppVersionInput.version	
+		}
+	})
+	return appVersion
+}
 module.exports = {
-	createAppVersion
+	createAppVersion,
+	editAppVersion
 }
