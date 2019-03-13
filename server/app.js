06dbc434dcede376a3b80aaff3e73b199622455c
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-13 15:42:21 +0100

    Serving static files

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
 const graphqlServer = require("graphql-yoga").GraphQLServer;
 const resolvers = require("./resolvers/resolvers");
 const { prisma } = require("./generated/prisma-client");
+const { static } = require("express");
+const path = require("path");
 
 const server = new graphqlServer({
 	typeDefs: "./schema.graphql", 
 	resolvers,
 	context: { prisma }
 });
+
+server.use('/static', static(path.join(__dirname, 'public')))
 
 server.start(() => console.log("Running on port 4000"));
