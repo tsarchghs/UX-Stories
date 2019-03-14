5c198d9344d8bc8507ae116cbca810743dd74bd4
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-14 20:39:32 +0100

    Set request limit to 1000mb

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ const resolvers = require("./resolvers/resolvers");
 const { prisma } = require("./generated/prisma-client");
 const { static } = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const server = new graphqlServer({
 	typeDefs: "./schema.graphql", 
 	resolvers,
 	context: { prisma }
 });
+
+server.express.use(bodyParser.urlencoded({limit:"1000mb"}))
+server.express.use(bodyParser.json({limit:"1000mb"}))
 
 server.use('/static', static(path.join(__dirname, 'public')))
 
