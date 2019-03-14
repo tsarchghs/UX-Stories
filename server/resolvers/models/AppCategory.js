219fad5405c83e00f762db6929e8478cbd140339
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-14 20:42:10 +0100

    Added already exist check

--- server/resolvers/models/AppCategory.js
+++ server/resolvers/models/AppCategory.js
@@ -3,6 +3,10 @@ const createAppCategory = async (root,args,context) => {
 	if (!args.appCategoryInput.name) {
 		throw new Error("Please check that all of your arguments are not empty!")
 	}
+	const exists = await context.prisma.appCategories({where:{name:args.appCategoryInput.name}});
+	if (exists.length){
+		throw new Error("Category already exists (use different name)");
+	}
 	const appCategory = await context.prisma.createAppCategory({
 		name: args.appCategoryInput.name
 	})
