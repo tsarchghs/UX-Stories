18f4cdbf2ed4df0cbb119ea7c605a46c110eab8e
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-16 22:17:19 +0100

    Created libraryPermission

--- server/resolvers/permissions.js
+++ server/resolvers/permissions.js
@@ -8,5 +8,18 @@ module.exports = {
 		if (role === "ADMIN" && (context.user.role === "MEMBER") ) {
 			throw new Error("Unauthorized");
 		}
+	},
+	libraryPermission: async (context,libraryId) => {
+		const libraries = await context.prisma.libraries({
+			where: {
+				id: libraryId,
+				createBy: {
+					id: context.user.id
+				}
+			}
+		})
+		if (!libraries.length) {
+			throw new Error(`Node with id ${libraryId} doesn't exist or unauthorized`);
+		}
 	}
 }
