e285c2e5241dc9dac9bcb6b67d8f4d52a3688a8d
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-16 22:17:44 +0100

    Used token to get user otherwise not logged in

--- server/app.js
+++ server/app.js
@@ -4,11 +4,36 @@ const { prisma } = require("./generated/prisma-client");
 const { static } = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
 const server = new graphqlServer({
 	typeDefs: "./schema.graphql", 
 	resolvers,
-	context: { prisma }
+	context: async req => {
+		var user = undefined
+		var loggedIn = false
+		if (req.request.headers["authorization"]){
+			const token = req.request.headers["authorization"].split(" ")[1];
+			const decoded = await jwt.verify(token,"secret_key",(err,decoded) => {
+				if (err) {
+					if (err.name === "JsonWebTokenError") {
+						return false
+					}
+					return new Error("Invalid token");
+				}
+				return decoded
+			});
+			if (decoded){
+				user = await prisma.user({id:decoded.userId});
+				loggedIn = true;
+			}
+		}
+		return {
+			prisma,
+			user,
+			loggedIn
+		}
+	}
 });
 
 server.express.use(bodyParser.urlencoded({limit:"1000mb",extended:true}))
