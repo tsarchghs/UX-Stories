b947b7890fc3e9bf4daa1cd440f6405409682cce
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-21 23:09:41 +0100

    Added support for user profile photos

--- server/resolvers/authentication.js
+++ server/resolvers/authentication.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const configs = require("../configs");
+const fileHandling = require("../modules/fileApi");
+
 const saltRounds = 10;
 
 const createToken = (userId) => {
@@ -30,18 +32,36 @@ const login = async (root,args,context) => {
 }
 
 const signUp = async (root,args,context) => {
-	console.log(args);
+	var hasLogo; 
+	var profile_photo;
 	if (!args.email || !args.first_name || !args.last_name || !args.password){
 		throw new Error("Please check that all of your arguments are not empty!")
 	}
+	if (args.profile_photo && !(args.profile_photo.base64 && args.profile_photo.mimetype)) {
+		throw new Error("profile_photo.base64 or profile_photo.mimetype is missing");
+	} else {
+		hasLogo = args.profile_photo ? true : false
+	}
+	if (hasLogo){
+		profile_photo = await fileHandling.processUpload(args.profile_photo.base64,
+															   args.profile_photo.mimetype,
+															   context);
+		console.log(profile_photo);
+	}
 	const hashed_password = await bcrypt.hash(args.password,saltRounds);
-	const user = await context.prisma.createUser({
+	var userParams = {
 		email: args.email,
 		first_name: args.first_name,
 		last_name: args.last_name,
 		password: hashed_password,
 		role: "MEMBER"
-	});
+	}
+	if (profile_photo){
+		userParams["profile_photo"] = {
+			connect: { id: profile_photo.id }
+		}
+	}
+	const user = await context.prisma.createUser(userParams);
 	return {
 		userId: user.id,
 		token: createToken(user.id),
