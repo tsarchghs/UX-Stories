0e6d442243065a6b12a9ee79ffb35d5d151a6023
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-22 00:09:09 +0100

    Updates profile picture when user loaded

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -21,7 +21,7 @@ class Header extends React.Component {
 		                <p className="light-gray"><a href="#">Upgrade</a></p>
 		                <div>
 			                <Link to="/profile">
-			                	<img className="profile-nav__img" src="https://scontent.fprn1-1.fna.fbcdn.net/v/t1.0-1/p160x160/35988964_1041356179350456_5301902918050381824_n.jpg?_nc_cat=105&_nc_ht=scontent.fprn1-1.fna&oh=2142e132de1a494501a3fc90d51e365c&oe=5D4FB1D7"/>
+			                	<img className="profile-nav__img" src={this.props.user && this.props.user.profile_photo ? this.props.user.profile_photo.url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOo9ftjYQCU8HW1YByx0oAQdegRxO51mQN0tKKenGRnDZb-_D6"} alt />
 		              		</Link>
 		              	</div>
 		              </div>    
