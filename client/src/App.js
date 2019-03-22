7ac28cf3af332578c82ba6a13767474d6c39c37b
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-22 16:18:58 +0100

    Implemented login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,77 +5,138 @@ import gql from "graphql-tag";
 import Profile from "./components/profile";
 import Home from "./components/home";
 import Header from "./components/header";
+import Login from "./components/login";
+import Cookies from "js-cookie";
 
-const client = new ApolloClient({
+
+var client = new ApolloClient({
     uri: "http://localhost:4000/",
     headers: {
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjanRqN21ocHk2MzBqMGI5MXJucG1zNzlzIiwiaWF0IjoxNTUzMjA3NDY5fQ.YyMqWk8zRE_V02Ic9leg5kuffj8samTQoRdSe4a655w"
+      "Authorization": `Bearer ${Cookies.get("auth_token")}`
     }
 })
 
-// client.query({
-//     query: gql`
-//         query {
-//             storyCategories {
-//                 id
-//             }
-//         }
-//     `
-// }).then(result => console.log(result));
-
-// const Index = () => {
-//     return <h2>Home</h2>
-// }
-// const About = () => {
-//     return <h2>About</h2>
-// }
-// const Users = () => {
-//     return <h2>Users</h2>
-// }
-// const activeStyle = {
-//     fontWeight: "bold",
-//     color: "red"
-// } 
+// const client = new ApolloClient({
+//     uri: "http://localhost:4000/",
+//     headers: {
+//       "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjanRqN21ocHk2MzBqMGI5MXJucG1zNzlzIiwiaWF0IjoxNTUzMjA3NDY5fQ.YyMqWk8zRE_V02Ic9leg5kuffj8samTQoRdSe4a655w"
+//     }
+// })
 
 class App extends Component {
     constructor(props){
         super(props);
         this.state = {
-            user: {}
+            user: undefined,
+            show_message: ""
         }
+        this.login = this.login.bind(this);
+        console.log(this.state.user,111);
     }
     async componentDidMount() {
-        const data = await client.query({
-            query: gql`
-                query {
-                    getLoggedInUser{
-                    first_name
-                    last_name
-                    email
-                    profile_photo {
-                      url
+        try {
+            var data = await client.query({
+                    query: gql`
+                        query {
+                            getLoggedInUser{
+                            first_name
+                            last_name
+                            email
+                            profile_photo {
+                              url
+                            }
+                          }
+                        }
+                    `
+                });
+        } catch (e) {
+            if (e.message === "GraphQL error: Not logged in"){
+                this.setState({
+                    user: null
+                })
+            }
+        }
+        if (data){
+            let user = data.data.getLoggedInUser
+            this.setState({
+                user: user
+            })
+        }
+    }
+    async login(e) {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        try {
+            var data = await client.query({
+                query: gql`
+                    query {
+                        login(
+                            email: "${email}",
+                            password: "${password}"
+                        ) {
+                            token
+                        }
                     }
-                  }
+                `
+            })
+        console.log(data);
+        } catch (e) {
+            if (e.message === "GraphQL error: Invalid credentials") {
+                this.setState({
+                    show_message:"Invalid credentials"
+                });
+            }
+            console.log(e);
+        }
+        if (data) {
+            Cookies.set("auth_token",data.data.login.token);
+            client = new ApolloClient({
+                uri: "http://localhost:4000/",
+                headers: {
+                  "Authorization": `Bearer ${data.data.login.token}`
                 }
-            `
-        })
-        console.log(data.data.getLoggedInUser);
-        this.setState({
-            user: data.data.getLoggedInUser
-        })
+            })
+            this.componentDidMount();
+        }
     }
     render(){
         return (
             <Router>
-                <Header user={this.state.user} />
-                <Route path="/" exact component={() => {
-                    return <Home user={this.state.user} />
-                }} />
-                <Route path="/profile" component={() => {
-                    return (
-                        <Profile user={this.state.user} client={client} />
-                    );
-                }} />
+                {
+
+                    this.state.user === undefined ?                       
+                    (
+                        <center style={{margin:300}}>
+                            <img src="https://loading.io/spinners/rolling/lg.curve-bars-loading-indicator.gif" />
+                        </center>
+                    )
+
+                    :
+                    (
+                        this.state.user === null ?
+                        (
+                            <Login login={this.login} show_message={this.state.show_message}/>
+                        ) 
+
+                        :
+                        
+                        (    
+                            <div>
+                                <Header user={this.state.user} />
+                                            <Route path="/" exact component={() => {
+                                                return <Home user={this.state.user} />
+                                            }} />
+                                            <Route path="/profile" component={() => {
+                                                return (
+                                                    <Profile user={this.state.user} client={client} />
+                                                );
+                                            }} />
+                            </div>
+                        )
+                    )
+
+                }
 
                 <link rel="stylesheet" href="../assets/toolkit/styles/toolkit.css" />
                 <script src="../assets/toolkit/scripts/jquery.min.js"></script>
