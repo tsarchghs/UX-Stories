1dc9e16025efcb0d337f480f639aea1d19575bfc
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-22 21:24:00 +0100

    Shows all stories by default, added support for searching by name

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,6 +1,57 @@
 import React from "react";
+import Loading from "./loading";
+import gql from "graphql-tag";
 
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      stories: undefined
+    }
+    this.search = this.search.bind(this);
+  }
+  async componentDidMount() {
+    let results = await this.props.client.query({
+      query: gql`
+        query {
+          stories {
+            id
+            thumbnail {
+              url
+            }
+          }
+        }
+      `
+    })
+    let stories = results.data.stories;
+    console.log(stories);
+    this.setState({
+      stories: stories
+    })    
+  }
+  async search(e) {
+    this.setState({
+      stories: undefined
+    })
+    let results = await this.props.client.query({
+      query: gql`
+        query {
+          stories(storiesFilterInput:{
+            appName_contains:"${e.target.value}"
+          }){
+            id
+            thumbnail {
+              url
+            }
+          }
+        }
+      `
+    })
+    let stories = results.data.stories;
+    this.setState({
+      stories: stories
+    })
+  }
   render() {
     return (
       <div>
@@ -12,7 +63,7 @@ class Home extends React.Component {
                 <span className="seperator" />
                 <div className="search">
                   <img src="/assets/toolkit/images/search-icon.svg" alt />
-                  <input type="text" placeholder="Search by app name..." />
+                  <input onChange={this.search} type="text" placeholder="Search by app name..." />
                 </div>      </div>
               <div className="flex">
                 <div className="filter">
@@ -28,7 +79,7 @@ class Home extends React.Component {
         <div className="results">
           <div className="container">
             <div className="results__content">
-              <p className="results__results bold">Showing 6 Results</p>
+              <p className="results__results bold">Showing {this.state.stories ? this.state.stories.length : 0} Results</p>
               <div className="ux-label ">
                 <p className="light-gray">Social networking</p>
                 <span><img src="/assets/toolkit/images/008-delete.svg" alt /></span>
@@ -44,12 +95,23 @@ class Home extends React.Component {
         </div><div className="cards">
           <div className="container">
             <div className="cards__content">
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
-              <a href="#"><img src="../../assets/toolkit/images/bitmap.jpg" alt /></a>
+            {
+              this.state.stories && !this.state.stories.length ? <center>{"Nothing to show"}</center> : ""
+            }
+            {
+              this.state.stories === undefined ?
+              (
+                <Loading />
+              ) 
+
+              :
+              
+              (
+                this.state.stories.map(story => 
+                  <a href="#" key={story.id}><img style={{width:300,height:600}} key={story.id} src={story.thumbnail.url} alt /></a>
+                )
+              )
+            }
             </div>
           </div>
         </div>
