c34da28c50561ad51bae6d7636da628b6b56de56
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-29 14:07:45 +0100

    Created E404 component

---
+++ client/src/components/E404.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class E404 extends React.Component {
+	render() {
+		return (
+	      <div id="notfound">
+	        <div className="notfound">
+	          <div className="notfound-404">
+	            <h1>404</h1>
+	          </div>
+	          <h2>Oops, The Page you are looking for can't be found!</h2>
+	          {/*<form className="notfound-search">
+	            <input type="text" placeholder="Search..." />
+	            <button type="button">Search</button>
+	          </form>
+	          <a href="#"><span className="arrow" />Return To Homepage</a>*/}
+	        </div>
+	      </div>
+	    );
+	}
+}
+
+export default E404;
