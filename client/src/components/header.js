a5222595c0be26e54ba4756b7c3d9aee94896804
Gjergj Kadriu <gjergjk71@gmail.com>
2019-03-29 14:09:03 +0100

    Removed alt

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,11 +8,11 @@ class Header extends React.Component {
 	          <div className="container">
 	            <div className="header__content">
 		              <div className="logo">
-		                <a href="#"><Link to="/"><img src="/assets/toolkit/images/logo.svg" alt /></Link></a>
+		                <Link to="/"><img src="/assets/toolkit/images/logo.svg"/></Link>
 		              </div>        <div className="nav">
 		                <h5 className="light-gray"><a href="#">Apps</a></h5>
 		                <Link to="/">
-		               		<h5 className="light-gray active"><a href="#">Stories</a></h5>
+		               		<h5 className="light-gray active">Stories</h5>
 		               	</Link>
 		                <h5 className="light-gray"><a href="#">Jobs</a></h5>
 		              </div>        <div className="profile-nav">
@@ -21,7 +21,7 @@ class Header extends React.Component {
 		                <p className="light-gray"><a href="#">Upgrade</a></p>
 		                <div>
 			                <Link to="/profile">
-			                	<img className="profile-nav__img" src={this.props.user && this.props.user.profile_photo ? this.props.user.profile_photo.url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOo9ftjYQCU8HW1YByx0oAQdegRxO51mQN0tKKenGRnDZb-_D6"} alt />
+			                	<img className="profile-nav__img" src={this.props.user && this.props.user.profile_photo ? this.props.user.profile_photo.url : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOo9ftjYQCU8HW1YByx0oAQdegRxO51mQN0tKKenGRnDZb-_D6"} />
 		              		</Link>
 		              	</div>
 		                <p className="light-gray"><a onClick={this.props.user.logout}>Logout</a></p>
