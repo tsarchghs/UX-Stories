ec12eb4ca9bbbd9440e3df009f76db29ff06a3b7
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-04 22:55:50 +0200

    Defined getQueryParams

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -80,11 +80,25 @@ const insertActiveFilters = (filters,state) => {
     return filters;
 }
 
+var getQueryParams = function (url) {
+  var params = {};
+  var parser = document.createElement('a');
+  parser.href = url;
+  var query = parser.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    params[pair[0]] = decodeURIComponent(pair[1]);
+  }
+  return params;
+};
+
 export {
 	getStories,
 	getAppCategories,
 	getStoryCategories,
 	getStoryElements,
   getActiveFilters,
-  insertActiveFilters
+  insertActiveFilters,
+  getQueryParams
 }
