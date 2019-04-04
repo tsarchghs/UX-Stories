810163830affb94c72c13af634252e5dae9f47d7
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-04 12:02:37 +0200

    forgot password token configs and mailgun configs

--- server/configs.js
+++ server/configs.js
@@ -1,4 +1,13 @@
 
 module.exports = {
-	jwt_secret: "secret_very_secret"
+	jwt_secret: "secret_very_secret",
+	jwt_forgotPassword_secret: "forgot_password_token_secret",
+	auth: {
+		user: "gjergjk71@gmail.com",
+		pass: "gjergji.123"
+	},
+	mailgun:{
+		apiKey:'36b77ddc5691055f9e2d350ab165913e-2416cf28-0ca40814',
+		DOMAIN:'sandbox7c10cba56e9a4f0f9b23c09194475167.mailgun.org'
+	}
 }
