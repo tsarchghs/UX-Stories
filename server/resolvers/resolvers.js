6ecaed32f44c94793da9629f44231ac84a5d085a
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-07 20:30:43 +0200

    mapped story

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -18,6 +18,7 @@ module.exports = {
 		apps: appResolvers.apps,
 		appCategories: appCategoryResolvers.appCategories,
 		appVersions: appVersionResolvers.appVersions,
+		story: storyResolvers.story,
 		stories: storyResolvers.stories,
 		storyCategories: storyCategoryResolvers.storyCategories,
 		storyElements: storyElementResolvers.storyElements,
