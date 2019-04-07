dd8c140c10cf1ebf9df1df2e2a0f60b5e7020a49
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-07 20:32:03 +0200

    Created SingleStory

---
+++ client/src/components/singleStory.js
@@ -0,0 +1,196 @@
+import React from "react";
+import SingleStoryAppLoading from "./singleStoryAppLoading";
+import gql from "graphql-tag";
+import { Link } from "react-router-dom";
+import { getQueryParams } from "../helpers";
+
+class SingleStory extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			app: props.app ? props.app : undefined,
+			storyCategories: undefined,
+			storyElements: undefined 
+		}
+		this.story_id = undefined;
+		let param_id = props.match.match.params.id
+		if (param_id[param_id.length - 1] === "#"){
+			this.story_id = param_id.slice(0,param_id.length-1);
+		} else {
+			this.story_id = param_id
+		}
+	}
+	async componentDidMount(){
+		if (!this.state.app){
+			console.log(this.story_id,this.props.match.match.params.id)
+			let data = await this.props.client.query({
+				query: gql`
+					query {
+						story(id:"${this.story_id}"){
+						    id
+							app {
+								id
+								name
+								description
+								logo {
+									id
+									url
+								}
+							}
+						    storyElements {
+						    	id
+						    	name
+						    }
+						    storyCategories {
+						    	id
+						    	name
+						    }
+						    video {
+						    	id
+						    	file {
+						    		id
+						    		url
+						    		mimetype
+						    	}
+						    }
+					  }
+					}
+				`
+			})
+			this.setState({
+				app: data.data.story.app,
+				storyElements: data.data.story.storyElements,
+				storyCategories: data.data.story.storyCategories,
+				video: data.data.story.video.file
+			},() => console.log(this.state))
+			console.log(data);
+		}
+	}
+	render(){
+		let params = getQueryParams(window.location.href);
+		let redirect_back = this.state.app ? `/app/${this.state.app.id}` : "#"
+		if (params["from"] === "stories"){
+			redirect_back = "/stories";
+		}
+		return (
+			<div className="inside-stories">
+		        <div className="inside-stories__container">
+		          <Link to={redirect_back}>
+		          	<span className="inside-stories__close"><img src="/assets/toolkit/images/006-error.svg" alt /></span>
+		          </Link>
+		          <div className="inside-stories__top">
+		          {
+		          	!this.state.app ? <SingleStoryAppLoading/>
+		          	: 
+		          		<div className="apps__top">
+				              <div className="apps__top-image" style={{backgroundImage: 'url("/assets/toolkit/images/netflix-logo.jpg")'}} />
+				              <div className="apps__top-info">
+				                <h5 className="bold">{this.state.app.name}</h5>
+				                <p className="apps__small-title light-gray">{this.state.app.description}</p>
+				              </div>
+					  	</div> 
+		          }
+		          {
+		          	this.state.app 
+		          	? <Link to={redirect_back}>
+		            	<p className="pink">See other stories</p>
+		            </Link>
+		            :  <a href="#"><p className="pink">See other stories</p></a>
+		          }
+		          </div>
+		          <div className="inside-stories__hero">
+		            <div className="inside-stories__video">
+		            {
+		            	!this.state.video
+		            	? <img style={{borderRadius:25,width: 230}} src="/assets/toolkit/images/loadingVideo.png"/>
+		            	: <video style={{borderRadius:25}} controls width="230" height="500">
+
+						    <source src={this.state.video}
+						            type={`video/${this.state.video.mimetype}`}/>
+						 </video>
+		            }
+		            </div>
+		            <div className="inside-stories__info">
+		              <div className="inside-stories__card">
+		                <div className="inside-stories__card--inside">
+		                  <p className="bold">Screens</p>
+		                  {
+		                  	this.state.storyCategories && !this.state.storyCategories.length
+		                  	? <p>Nothing to show</p>
+		                  	: ""
+		                  }
+		                  {
+		                  	!this.state.storyCategories ? 
+		                  	<div>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  	</div>
+		                  	: this.state.storyCategories.map(storyCategory => {
+		                  		return (
+				                  <div className="ux-label no-close">
+				                    <p className="light-gray">{storyCategory.name}</p>
+				                    <span><img src="/assets/toolkit/images/008-delete.svg" alt /></span>
+				                  </div>
+		                  		);
+		                  	})
+		                  }				
+		                 </div>
+		                <div className="inside-stories__card--inside">
+		                  <p className="bold">Elements</p>
+		                  {
+		                  	this.state.storyElements && !this.state.storyElements.length
+		                  	? <p>Nothing to show</p>
+		                  	: ""
+		                  }
+		                  {
+		                  	!this.state.storyElements ? 
+		                  	<div>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  		<img style={{paddingRight:5}} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAE8AAAAgCAYAAABTliUJAAAAfUlEQVRoQ+3UsQ3AMAwEMXv/fdUmgEfQtVR/DfHQnZnvuJXAhbdyexG8vR28YAcPXhEIrZ8HLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEgtD14QCKnlwQsCIbU8eEEgpJYHLwiE1PLgBYGQWh68IBBSy4MXBEJqefCCQEh/JfZ8Qf2qHe8AAAAASUVORK5CYII="/>
+		                  	</div>
+		                  	: this.state.storyElements.map(storyElement => {
+		                  		return (
+				                  <div className="ux-label no-close">
+				                    <p className="light-gray">{storyElement.name}</p>
+				                    <span><img src="/assets/toolkit/images/008-delete.svg" alt /></span>
+				                  </div>
+		                  		);
+		                  	})
+		                  }					
+		                 </div>
+		                <hr />
+		                <div className="inside-stories__card--share">
+		                  <p className="bold">Share</p>
+		                  <div>
+		                    <a href="#"><img src="/assets/toolkit/images/fb.svg" alt /></a>
+		                    <a href="#"><img src="/assets/toolkit/images/twt.svg" alt /></a>
+		                    <a href="#"><img src="/assets/toolkit/images/pt.svg" alt /></a>
+		                    <a href="#"><img src="/assets/toolkit/images/mail.svg" alt /></a>
+		                  </div>
+		                </div>
+		                <hr />
+		                <div className="inside-stories__card--save">
+		                  <p className="bold">Save to libraries</p>
+		                  <button className="button">Select library</button>
+		                </div>
+		              </div>
+		              <div className="inside-stories__card small">
+		                <p className="bold">Sponsored</p>
+		                <div className="inside-stories__card--sponsored">
+		                  <div className="sponsored-card">
+		                    <img src="/assets/toolkit/images/hdhd.svg" alt />
+		                  </div>
+		                  <p className="light-gray bold">Plan your team’s availabilty and we will estimate when work starts. Also manage projects with stories.</p>
+		                </div>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		);
+	}
+}
+
+export default SingleStory;
