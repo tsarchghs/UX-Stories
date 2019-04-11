2361b12aae47f9d5730bc477175b02f2636d7037
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-11 16:15:30 +0200

    Created AppCategoriesDropdown

---
+++ client/src/components/appCategoriesDropdown.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import DropdownLoading from "./dropdownLoading";
+
+class AppCategoriesDropdown extends React.Component {
+	render(){
+		return (
+	        <div className="filter" id={this.props.id} data-dropdown data-auto-focus="true">
+	            <Query
+	              query={gql`
+	                query {
+	                  appCategories{
+	                    id
+	                    name
+	                  }
+	                }
+	            `}>
+	            { ({loading,error,data}) => {
+	              if (error) return <p>{error.message}</p>
+	              if (loading) return <DropdownLoading/>
+	              return (
+	                  <div className="filter-dropdown">
+	                  <div className="filter-dropdown__top">
+	                    <h5 className="gray bold">Filter with categories</h5>
+	                    <p className="pink">{this.props.filterBy.appCategory && (this.props.filterBy.appCategory !== "all") ? 1 : 0} selected</p>
+	                  </div>
+	                  <div className="filter-dropdown__main">
+
+	                        <label className="radio-t rde">
+	                          <label id={"all"+"_label"} className="gray bold">all</label>
+	                          <input 
+	                            className="ic" 
+	                            type="radio" 
+	                            name={1}
+	                            value={1}
+	                            onClick={this.props.handleAllFilterClick}
+	                          />
+	                          <span id="allCategoriesFilter" className="checkmark"/>
+	                        </label>
+	                    {
+	                      data.appCategories.map(appCategory => {
+	                        return (
+
+	                          <label className="radio-t rde">
+	                            <label id={appCategory.id+"_label"} className="gray bold">{appCategory.name}</label>
+	                            <input 
+	                              className="ic" 
+	                              type="radio" 
+	                              name={1}
+	                              value={1}
+	                              onClick={(e) => this.props.handleFilterClick(e,appCategory)}
+	                            />
+	                            <span className="checkmark"/>
+	                          </label>
+	                        );
+	                      })
+	                    } 
+	                  </div>
+	                </div>
+
+	              )
+	            }}
+	          </Query>
+	        </div>  
+		);
+	}
+}
+export default AppCategoriesDropdown;
