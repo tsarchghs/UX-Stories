d8d88459a17c1efa97da69f53ce8a63f99fd577b
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-16 19:00:13 +0200

    Created getObjectConnection

---
+++ resolvers/general.js
@@ -0,0 +1,34 @@
+
+const getObjectConnection = async (parent,args,context,info) => {
+	let filterBy = {where:{}}
+	for (var x in args.filterBy){
+		obj = args.filterBy[x];
+		filterBy[obj.key] = obj.value_str !== undefined ? obj.value_str : obj.value_int 
+	}
+	for (var x in args.where){
+		obj = args.where[x];
+		filterBy["where"][obj.key] = obj.value_str !== undefined ? obj.value_str : obj.value_int 
+	}
+	let data = await context.db.query[args.connection_type](filterBy,`
+	    {
+	    	edges {
+		      node {
+					${args.fields.join()}
+		    	}
+		    }
+		    aggregate {
+		      count
+		    }
+		    pageInfo{
+		      hasNextPage
+		      hasPreviousPage
+		    }
+		}
+	`);
+	data.nodes = { repr : JSON.stringify(data.edges.map(edge => edge.node)) };
+	return data;
+}
+
+module.exports = {
+	getObjectConnection
+}
