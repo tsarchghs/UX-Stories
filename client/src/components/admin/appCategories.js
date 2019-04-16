0bea7f8ce36db2d5f1a18d20d24f67b41343c674
Gjergj Kadriu <gjergjk71@gmail.com>
2019-04-16 19:05:04 +0200

    Re-used general components

--- client/src/components/admin/appCategories.js
+++ client/src/components/admin/appCategories.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Header from "./header";
 import LeftSidebar from "./leftSidebar";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { Link } from "react-router-dom";
+import { debounce } from "lodash";
+import AdminListPage from "./general/AdminListPage";
 
 class AppCategories extends React.Component {
 	render(){
@@ -8,70 +13,13 @@ class AppCategories extends React.Component {
 			<div>
 				<Header/>
 				<LeftSidebar/>
-		      <div className="dashboard-wrapper">
-		        <div className="container-fluid dashboard-content">
-		          {/* ============================================================== */}
-		          {/* pageheader */}
-		          {/* ============================================================== */}
-		          <div className="row">
-		            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-		              <div className="page-header">
-		                <h2 className="pageheader-title">Create new App Category</h2>
-		                <div className="page-breadcrumb">
-		                  <nav aria-label="breadcrumb">
-		                    <ol className="breadcrumb">
-		                      <li className="breadcrumb-item"><a href="app-categories.html" className="breadcrumb-link">App Categories</a></li>
-		                      <li className="breadcrumb-item active" aria-current="page">Create new app-category</li>
-		                    </ol>
-		                  </nav>
-		                </div>
-		              </div>
-		            </div>
-		          </div>
-		          {/* ============================================================== */}
-		          {/* end pageheader */}
-		          {/* ============================================================== */}
-		          <div className="page-header">
-		          </div>
-		          <div className="row">
-		            <div className="offset-xl-2 col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
-		              <div className="row">
-		                <div className="col-md-8">
-		                  <div className="card">
-		                    <div className="card-header">
-		                      <h4 className="mb-0">Creating new App Category</h4>
-		                    </div>
-		                    <div className="col-md-12 mb-12" style={{marginTop: '30px'}}>
-		                      <label htmlFor="firstName">Name of the app-category</label>
-		                      <input type="text" className="form-control" id="firstName" placeholder defaultValue required />
-		                      <div className="invalid-feedback">
-		                        Valid first name is required.
-		                      </div>
-		                      <hr className="mb-12" />
-		                      <button className="btn btn-primary btn-lg btn-block" type="submit" style={{marginBottom: '20px'}}>Create this app category🥳</button>
-		                    </div>
-		                  </div>
-		                </div>
-		              </div>
-		            </div>
-		          </div>
-		          <div className="row offset-xl-2 col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
-		            <div className="card">
-		              <div className="card-header">
-		                <h4 className="d-flex justify-content-between align-items-center mb-0">
-		                  <span className="text-muted">Delete this story</span>
-		                </h4>
-		              </div>
-		              <div className="card-body">
-		                <label htmlFor="firstName">Are you sure you want to delete this story?</label>    
-		                <a href="#" className="btn btn-secondary">Delete it 😭</a>
-		              </div>
-		            </div>
-		          </div>
-		          {/* ============================================================== */}
-		        </div>
-		      </div>
-			</div>
+				<AdminListPage 
+					typename="app categories" 
+					connection_type="appCategoriesConnection"
+					fields={["id","name"]}
+					first={5}
+				/>
+		   </div>
 		);
 	}
 }
--- client/src/components/admin/storyCategories.js
+++ client/src/components/admin/storyCategories.js
@@ -1,137 +1,26 @@
 import React from "react";
 import Header from "./header";
 import LeftSidebar from "./leftSidebar";
-
+import { Link } from "react-router-dom";
+import Table from "./general/Table";
+import AdminListPage from "./general/AdminListPage";
 
 class StoryCategories extends React.Component {
 	render(){
 		return (
 			<div>
-               <Header/>
-               <LeftSidebar/>
-		      <div className="dashboard-wrapper">
-		        <div className="container-fluid dashboard-content">
-		          <div className="row">
-		            <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-		              <div className="page-header">
-		                <h2 className="pageheader-title">Stories-Categories </h2>
-		                <p className="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
-		                <div className="page-breadcrumb">
-		                  <nav aria-label="breadcrumb">
-		                    <ol className="breadcrumb">
-		                      <li className="breadcrumb-item"><a href="#" className="breadcrumb-link">Dashboard</a></li>
-		                      <li className="breadcrumb-item"><a href="#" className="breadcrumb-link">Pages</a></li>
-		                      <li className="breadcrumb-item active" aria-current="page">Apps</li>
-		                    </ol>
-		                  </nav>
-		                </div>--&gt;
-		              </div>
-		            </div>
-		          </div>
-		          <div className="page-header">
-		            <div className="input-group col-xl-3 col-lg-12 col-md-12 col-sm-12 col-12">
-		              <input type="text" className="form-control" placeholder="Search stories-categories" />
-		              <div className="input-group-append">
-		                <button type="submit" className="btn btn-primary">Search</button>
-		              </div>
-		              <div className="row">
-		                <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-		                  <div className="card">
-		                    <div style={{marginTop: '20px', marginRight: '20px'}}><a href="create-stories-categories.html" className="btn btn-primary float-right">Add new stories-category</a>
-		                      <h5 className="card-header">List of all stories-category</h5>
-		                    </div>
-		                    <div className="card-body">
-		                      <table className="table">
-		                        <thead>
-		                          <tr>
-		                            <th scope="col">#</th>
-		                            <th scope="col">Name</th>
-		                            <th scope="col">Category</th>
-		                            <th scope="col"> </th>
-		                          </tr>
-		                        </thead>
-		                        <tbody>
-		                          <tr>
-		                            <th scope="row">1</th>
-		                            <td>Facebook app</td>
-		                            <td>Social Networking</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                          <tr>
-		                            <th scope="row">2</th>
-		                            <td>iNZDR</td>
-		                            <td>Social Networking</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                          <tr>
-		                            <th scope="row">3</th>
-		                            <td>Quora</td>
-		                            <td>News</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                          <tr>
-		                            <th scope="row">4</th>
-		                            <td>Slack</td>
-		                            <td>Work tool</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                          <tr>
-		                            <th scope="row">5</th>
-		                            <td>AliExpress</td>
-		                            <td>Shopping</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                          <tr>
-		                            <th scope="row">6</th>
-		                            <td>Busulla</td>
-		                            <td>Education</td>
-		                            <td><div className="dd-nodrag btn-group ml-auto">
-		                                <button className="btn btn-sm btn-outline-light">Edit</button>
-		                                <button className="btn btn-sm btn-outline-light">
-		                                  <i className="far fa-trash-alt" />
-		                                </button>
-		                              </div></td>
-		                          </tr>
-		                        </tbody>
-		                      </table>
-		                    </div>
-		                  </div>
-		                </div>  
-		              </div>
-		              <div>
-		              </div>
-		            </div>
-		          </div>
-		        </div>
-		      </div>
-			</div>
+				<Header/>
+				<LeftSidebar/>
+				<AdminListPage
+					typename="story categories" 
+					connection_type="storyCategoriesConnection"
+					fields={["id","name"]}
+					first={5}
+				/>
+		   </div>
 		);
 	}
 }
+
 
 export default StoryCategories;
