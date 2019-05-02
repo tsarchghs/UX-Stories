cbc6f7fb7ae1deb054dfddc0f8f304cd3740fdc5
Gjergj Kadriu <gjergjk71@gmail.com>
2019-05-02 22:42:02 +0200

    New build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51090bc99b1520fddc6b11983096db18.js"
+  "/precache-manifest.2710d939ce14c50356ad9383c52ffc6c.js"
 );
 
 workbox.clientsClaim();
