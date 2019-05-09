87a2161311f7336b1121d514fd26e973e83d53b8
Gjergj Kadriu <gjergjk71@gmail.com>
2019-05-09 14:28:06 +0200

    Algolia Indexes

---
+++ algolia/indexes.js
@@ -0,0 +1,21 @@
+const client = require("./client");
+
+const appsIndex = client.initIndex("apps_index");
+const storiesIndex = client.initIndex("stories_index");
+
+appsIndex.setSettings({
+    searchableAttributes:[
+        "name"
+    ]
+})
+
+storiesIndex.setSettings({
+    searchableAttributes: [
+        "storyCategories.name"
+    ]
+})
+
+module.exports = {
+    appsIndex,
+    storiesIndex
+}
