8f2907c57615878a10f37be22f294d63d1bab503
Gjergj Kadriu <gjergjk71@gmail.com>
2019-05-13 20:23:25 +0200

    Opens edit modal

--- client/src/components/libraryCard.js
+++ client/src/components/libraryCard.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import $ from "jquery";
+import { loadToolkit } from "../helpers";
 
 class LibraryCard extends React.Component {
-  componentDidUpdate(){
-    $('.show-dropdown').click(function () {
-      $(this).toggleClass('close');
-    });
-  }
   componentDidMount(){
-    $('.show-dropdown').click(function () {
-      $(this).toggleClass('close');
-    });
+    loadToolkit()
+  }
+  componentDidUpdate(){
+    loadToolkit();
   }
   toggleDropDown(e){
+    console.log(1,e.target);
     $(e.target).toggleClass('close');
-  } 
+    $(e.target.parentElement).toggleClass('close');
+  }
   render() {
     return (
         <div className="libraries-card">
@@ -24,7 +23,7 @@ class LibraryCard extends React.Component {
             <div onClick={this.toggleDropDown} className="show-dropdown close" id={`toggleDropDown_${this.props.library.id}`}>
               <img src="../../assets/toolkit/images/edit.svg" className="pop-ed" alt />
               <div className="edit-dropdown">
-                <a href="#"><img src="../../assets/toolkit/images/edit-p.svg" alt /><p className="bold">Edit</p></a>
+                <a data-open={`editLibrary_${this.props.library.id}`}><img src="../../assets/toolkit/images/edit-p.svg" alt /><p className="bold">Edit</p></a>
                 <a href="#"><img src="../../assets/toolkit/images/edit-p.svg" alt /><p className="bold">Delete</p></a>
               </div>
             </div>
