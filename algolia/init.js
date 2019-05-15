4ecd2ccd9df0c9ef5327fe7c535fb1dfc795bce9
Gjergj Kadriu <gjergjk71@gmail.com>
2019-05-15 23:12:43 +0200

    Deletes objects in index but not in db

--- algolia/init.js
+++ algolia/init.js
@@ -20,14 +20,25 @@ const init = async () => {
         let objectsBrowser = info.index.browseAll()
         objectsBrowser.on("result",async data => {
             let all_objects = await prismaDb.query[info.query]({},info.prismaInfo);
-            let objectsOnIndex = data.hits.map(hit => hit.id);
+            let objectsOnIndex = data.hits.map(hit => hit.objectID);
             let new_objects = []
+            // add objects not in index currently
             all_objects.map(obj => {
                 if (objectsOnIndex.indexOf(obj.id) === -1){
+                    obj.objectID = obj.id
                     new_objects.push(obj);
                 }
             })
+            // delete objects that are in index but not in db
+            let deleteObjects = []
+            all_objects_ids = all_objects.map(obj => obj.id);
+            objectsOnIndex.map(objectID => {
+                if (all_objects_ids.indexOf(objectID) === -1){
+                    deleteObjects.push(objectID)
+                }
+            })
             info.index.addObjects(new_objects);
+            info.index.deleteObjects(deleteObjects);
         })
     }
 }
