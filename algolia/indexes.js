01ec08e82c130aabb5001ae4ea44a6d7295d3d3e
Gjergj Kadriu <gjergjk71@gmail.com>
2019-07-20 15:27:31 +0200

    added "id" as faceting attribute

--- algolia/indexes.js
+++ algolia/indexes.js
@@ -8,6 +8,7 @@ appsIndex.setSettings({
         "name"
     ],
     attributesForFaceting: [
+        "id",
         "appCategory.name"
     ]
 })
@@ -17,6 +18,7 @@ storiesIndex.setSettings({
         "storyCategories.name"
     ],
     attributesForFaceting: [
+        "id",
         "app.id",
         "appVersions.name",
         "app.appCategory.name",
