f85f1a1004fe3f6b5c16eaa7f3d8a9c9cabbb107
Gjergj Kadriu <gjergjk71@gmail.com>
2019-07-23 11:27:38 +0200

    new build

---
+++ client/build/precache-manifest.9a2339369f4624279236830f41565e70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "14658d450e9950cd238f",
+    "url": "/static/js/main.5e940fdf.chunk.js"
+  },
+  {
+    "revision": "b949d8ecc9715604964c",
+    "url": "/static/js/2.4732cecb.chunk.js"
+  },
+  {
+    "revision": "b949d8ecc9715604964c",
+    "url": "/static/css/2.33362bf7.chunk.css"
+  },
+  {
+    "revision": "5f43d788ed5ea79735d8696dd4045c32",
+    "url": "/index.html"
+  }
+];
