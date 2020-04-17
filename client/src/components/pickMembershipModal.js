b18fcf5f768c5ab3b85bca7438d41374c072ad4c
Gjergj Kadriu <gjergjk71@gmail.com>
2020-04-17 20:13:19 +0200

    Added front end to subscription modal

--- client/src/components/pickMembershipModal.js
+++ client/src/components/pickMembershipModal.js
@@ -5,8 +5,11 @@ import Modal from "react-responsive-modal";
 
 const customStyles = {
   overlay: {
-    paddingTop: "25%",
+    paddingTop: "5%",
     backgroundColor: "rgba(10, 10, 10, 0.75)"
+  },
+  modal:{
+      borderRadius: 16
   }
 };
 
@@ -25,20 +28,37 @@ class PickMembershipModal extends React.Component {
         contentLabel="Example Modal"
         type="fadeIn"
         styles={customStyles}
+        classNames={{ modal: "card-layout toggle"}}
       >
-        <div>
-            <h3 className="modal__title">Yearly membership</h3>
-            <div>
-            <Link to={{
-                pathname: "/payment",
-                state: { email: this.props.email }
-            }}>
-              <button>Continue</button>
-            </Link>
+          <div className="card">
+            <div className="card__header">
+              <div className="logo">Logo</div>
+              <div onClick={this.props.closeModal} className="close">X</div>
             </div>
-            <button className="close-button" data-close aria-label="Close reveal" type="button">
-              <img src="../../assets/toolkit/images/006-error.svg" onClick={this.props.closeModal} alt />
-            </button>
+            <div className="card__body">
+              <div className="title">Find real problems solved for millions of people</div>
+              <div className="subtitle">UXstories is hand picked collection of top apps on App Store that have the best practises of UX from login to purchasing a product and more.</div>
+              <div className="subs-card">
+                <div className="sub">
+                  <h4 style={{marginBottom:20,color: "white"}}>Pro Plan</h4>
+                  <h2 style={{marginBottom:5,color: "white"}}>$50</h2>
+                  <p style={{marginBottom:5,color: "white"}}>Yearly Subscription</p>
+                  <Link to={{
+                    pathname: "/payment",
+                    state: { email: this.props.email }
+                  }}>
+                   <button className="button">Subscribe</button>
+                  </Link>
+                </div>
+                <div className="plan">
+                  <p style={{color: "white"}}>200+ stories from top apps</p>
+                  <p style={{color: "white"}}>45+ apps</p>
+                  <p style={{color: "white"}}>Advanced Search & filtering</p>
+                  <p style={{color: "white"}}>Unlimited libraries</p>
+                </div>
+              </div>
+              <div className="stripe">Payments powered by stripe</div>
+            </div>
           </div>
         </Modal>
 		);
